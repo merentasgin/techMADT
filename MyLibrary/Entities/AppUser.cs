689cd8c29eb76ed9c282bf2ed3da55052dd08a25
Muhammed Eren TAŞGİN <merentasgin@gmail.com>
2025-05-17 03:25:01 +0300

    17.05

---
+++ MyLibrary/Entities/AppUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace techMADT.Core.Entities
+{
+    public class AppUser :IEntity
+
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; } 
+        public string? Phone { get; set; } 
+        public string Password { get; set; }
+
+        public string? UserName { get; set; }=string.Empty;
+
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+      
+
+        public Guid? UpdateDate { get; set; }=Guid.NewGuid();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ techMADT.Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using techMADT.Core.Entities;
+
+namespace techMADT.Data
+{
+    public class DatabaseContext :DbContext
+    {
+        public DbSet <AppUser> AppUsers{ get; set; }
+        public DbSet <Brand> Brands {get; set; }
+        public DbSet <Category> Categories{ get; set; }
+        public DbSet <Contact> Contacts{ get; set; }
+        public DbSet <News> News{ get; set; }
+        public DbSet <Product> Products{ get; set; }
+        public DbSet <Slider> Sliders{ get; set; }
+    }
+}
